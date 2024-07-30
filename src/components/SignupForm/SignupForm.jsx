ba605487d659f22c50a175aa37205458cbf304cb
Muhammad Kaif Nazeer <122388541+MuhammadKaifNazeer@users.noreply.github.com>
2024-07-30 07:38:46 +0000

    removed extra code from signup form component

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -117,50 +117,6 @@ const SignupForm = () => {
         </div>
       </div>
     </div>
-    // <div className="min-h-screen min-w-screen bg-zinc-900">
-    //   <div className="w-full min-h-screen bg-zinc-900 text-white p-10">
-    //     <h3 className="text-4xl mb-3 font-semibold">Register</h3>
-    //     <form
-    //       onSubmit={handleSubmit}
-    //       method="POST"
-    //       className="flex flex-col gap-3"
-    //     >
-    //       <input
-    //         className="px-3 py-2 bg-zinc-800 rounded-lg"
-    //         type="text"
-    //         placeholder="Name"
-    //         value={name}
-    //         onChange={(e) => setName(e.target.value)}
-    //       />
-    //       <input
-    //         className="px-3 py-2 bg-zinc-800 rounded-lg"
-    //         type="email"
-    //         placeholder="Email"
-    //         value={email}
-    //         onChange={(e) => setEmail(e.target.value)}
-    //       />
-    //       <input
-    //         className="px-3 py-2 bg-zinc-800 rounded-lg"
-    //         type="password"
-    //         placeholder="Password"
-    //         value={password}
-    //         onChange={(e) => setPassword(e.target.value)}
-    //       />
-    //       <LoaderButton loading={loading}>Register</LoaderButton>
-    //       {error && (
-    //         <div className="border rounded-lg broder-zinc-800 px-3 py-2 w-max">
-    //           {error}
-    //         </div>
-    //       )}
-    //     </form>
-    //     <p className="mt-3 text-lg">
-    //       Already have an account?{" "}
-    //       <Link href="/login" className="text-green-500 font-bold">
-    //         Login
-    //       </Link>
-    //     </p>
-    //   </div>
-    // </div>
   );
 };
 
