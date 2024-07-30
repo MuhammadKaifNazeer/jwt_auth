1769cca19f4a0ec464932a6681e8d7e9d06e8622
Muhammad Kaif Nazeer <122388541+MuhammadKaifNazeer@users.noreply.github.com>
2024-07-30 08:20:48 +0000

    update

--- src/app/page.js
+++ src/app/page.js
@@ -7,5 +7,50 @@ export default function Home() {
 
   router.push("/login");
 
-  return <>please wait...</>;
+  return (
+    <>
+      <Loader />
+    </>
+  );
 }
+
+const Loader = () => {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black z-50">
+      <div className="loader"></div>
+    </div>
+  );
+};
+
+const styles = `
+  .loader {
+    display: inline-block;
+    border: 5px solid rgba(255, 255, 255, 0.6);
+    border-top-color: rgba(255, 255, 255, 1);
+    border-radius: 50%;
+    width: 40px;
+    height: 40px;
+    animation: spin 1s linear infinite;
+  }
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+const injectStyles = () => {
+  if (typeof document !== "undefined") {
+    const styleSheet = document.createElement("style");
+    styleSheet.type = "text/css";
+    styleSheet.innerText = styles;
+    document.head.appendChild(styleSheet);
+  }
+};
+
+injectStyles();
